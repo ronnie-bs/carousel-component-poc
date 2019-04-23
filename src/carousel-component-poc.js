a7e71c63935aec9df425d1d79430b01ce2eaf4b6
Ronnie Mathew <ronnie@bricksimple.com>
2019-04-23 16:35:38 -0400

    Added README.MD

--- src/carousel-component-poc.js
+++ src/carousel-component-poc.js
@@ -4,5 +4,14 @@
 // }
 
 function showCardPositions() {
-    console.log('Card Positions');
+    var carouselContainerElem = document.getElementsByClassName('carousel-container')[0];
+    // carouselContainerElem.getBoundingClientRect().left;
+    console.log('carouselContainerElem', carouselContainerElem.getBoundingClientRect().left);
+
+    var cardContainerElem = document.getElementsByClassName('card-container');
+    for (var i = 0; i < cardContainerElem.length; i++) {
+        console.log('cardContainerElem[' + i + ']', cardContainerElem[i].offsetLeft, cardContainerElem[i].offsetTop);
+    }
+
+    console.log('---------------------');
 }

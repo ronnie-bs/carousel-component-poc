dfd531cc301f950043966ba18dd7d94b61d6d734
Ronnie Mathew <ronnie@bricksimple.com>
2019-04-24 01:04:14 -0400

    Implemented the scroll animation with edge fade

--- src/carousel-component-poc.js
+++ src/carousel-component-poc.js
@@ -1,17 +1,52 @@
-// function scrollHandler() {
-//     console.log('scrollHandler...');
-//     document.get
-// }
+function onLoad() {
+    computeCardPositions();
+}
 
-function showCardPositions() {
+function scrollHandler() {
+    computeCardPositions();
+}
+
+function computeCardPositions() {
     var carouselContainerElem = document.getElementsByClassName('carousel-container')[0];
-    // carouselContainerElem.getBoundingClientRect().left;
-    console.log('carouselContainerElem', carouselContainerElem.getBoundingClientRect().left);
+    const carouselBound = carouselContainerElem.getBoundingClientRect();
+    const caroundBoundRight = Math.trunc(carouselBound.right);
+    // console.log('caroundBoundRight', caroundBoundRight);
 
     var cardContainerElem = document.getElementsByClassName('card-container');
     for (var i = 0; i < cardContainerElem.length; i++) {
-        console.log('cardContainerElem[' + i + ']', cardContainerElem[i].offsetLeft, cardContainerElem[i].offsetTop);
-    }
+        // const pElem = cardContainerElem[i].getElementsByTagName('p')[0];
+        const cardContainerBound = cardContainerElem[i].getBoundingClientRect();
+        const cardContainerRelPos = Math.trunc(cardContainerBound.left + cardContainerBound.width);
 
-    console.log('---------------------');
+        if (cardContainerRelPos > caroundBoundRight) {
+            // console.log('cardContainerRelPos', i, cardContainerRelPos);
+            if (cardContainerBound.left <= caroundBoundRight) {
+                const pct = Math.trunc((caroundBoundRight - cardContainerBound.left) / cardContainerBound.width * 100);
+                console.log('pct', i, pct);
+                if (pct < 99 ) {
+                    // const offset = pct > 20 ? 20 : 5;
+                    const offset = pct <= 80 ? pct - 20 : (2 * pct - 105) ;
+                    console.log('offset', offset);
+                    cardContainerElem[i].style.background = 'unset';
+                    cardContainerElem[i].style.backgroundImage = 'linear-gradient(to right, white ' + offset + '%, rgba(255, 255, 255, 0) ' + pct + '%)';
+                    // pElem.style.maskImage = 'linear-gradient(to right, rgba(0,0,0,1) ' + (pct - 20) + ', rgba(0,0,0,0)' + pct + '%)';
+                    // pElem.style.webkitMaskImage = 'linear-gradient(to right, rgba(0,0,0,1) ' + (pct - 20) + ', rgba(0,0,0,0)' + pct + '%)';
+                } else {
+                    cardContainerElem[i].style.background = 'white';
+                    cardContainerElem[i].style.backgroundImage = 'unset';
+                    // pElem.style.maskImage = 'unset';
+                    // pElem.style.webkitMaskImage = 'unset';
+                }
+            }
+            // cardContainerElem[i].classList.add('last-card');
+        } else {
+            // cardContainerElem[i].classList.remove('last-card');
+            cardContainerElem[i].style.background = 'white';
+            cardContainerElem[i].style.backgroundImage = 'unset';
+            // pElem.style.maskImage = 'unset';
+            // pElem.style.webkitMaskImage = 'unset';
+        }
+    } 
+
+    // console.log('---------------------');
 }

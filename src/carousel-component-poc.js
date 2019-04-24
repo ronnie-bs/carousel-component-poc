a23859b583b24d0548cd00cf38980e1b775f4cb1
Ronnie Mathew <ronnie@bricksimple.com>
2019-04-24 09:25:35 -0400

    Applied the fading to the text in the card along with the background

--- src/carousel-component-poc.js
+++ src/carousel-component-poc.js
@@ -14,7 +14,7 @@ function computeCardPositions() {
 
     var cardContainerElem = document.getElementsByClassName('card-container');
     for (var i = 0; i < cardContainerElem.length; i++) {
-        // const pElem = cardContainerElem[i].getElementsByTagName('p')[0];
+        const pElem = cardContainerElem[i].getElementsByTagName('p')[0];
         const cardContainerBound = cardContainerElem[i].getBoundingClientRect();
         const cardContainerRelPos = Math.trunc(cardContainerBound.left + cardContainerBound.width);
 
@@ -22,22 +22,22 @@ function computeCardPositions() {
             // console.log('cardContainerRelPos', i, cardContainerRelPos);
             if (cardContainerBound.left <= caroundBoundRight) {
                 const pct = Math.trunc((caroundBoundRight - cardContainerBound.left) / cardContainerBound.width * 100);
-                console.log('pct', i, pct);
+                // console.log('pct', i, pct);
                 if (pct < 99 ) {
                     // const offset = pct <= 80 ? pct - 20 : (2 * pct - 105);
                     const offsetFactor = 30;
                     const offsetBuffer = 3;
                     const offset = pct <= (100 - offsetFactor) ? (pct - offsetFactor) : (pct - offsetFactor) + (pct - (100 - offsetFactor)) + offsetBuffer;
-                    console.log('offset', offset);
+                    // console.log('offset', offset);
                     cardContainerElem[i].style.background = 'unset';
                     cardContainerElem[i].style.backgroundImage = 'linear-gradient(to right, rgba(255, 255, 255, 1) ' + offset + '%, rgba(255, 255, 255, 0) ' + pct + '%)';
-                    // pElem.style.maskImage = 'linear-gradient(to right, rgba(0,0,0,1) ' + offset + ', rgba(0,0,0,0)' + pct + '%)';
-                    // pElem.style.webkitMaskImage = 'linear-gradient(to right, rgba(0,0,0,1) ' + offset + ', rgba(0,0,0,0)' + pct + '%)';
+                    pElem.style.maskImage = 'linear-gradient(to right, rgba(255, 255, 255, 1) ' + offset + '%, rgba(255 ,255, 255, 0)' + pct + '%)';
+                    pElem.style.webkitMaskImage = 'linear-gradient(to right, rgba(255, 255, 255, 1) ' + offset + '%, rgba(255, 255, 255, 0)' + pct + '%)';
                 } else {
                     cardContainerElem[i].style.background = 'rgba(255, 255, 255, 1)';
                     cardContainerElem[i].style.backgroundImage = 'unset';
-                    // pElem.style.maskImage = 'unset';
-                    // pElem.style.webkitMaskImage = 'unset';
+                    pElem.style.maskImage = 'unset';
+                    pElem.style.webkitMaskImage = 'unset';
                 }
             }
             // cardContainerElem[i].classList.add('last-card');
@@ -45,8 +45,8 @@ function computeCardPositions() {
             // cardContainerElem[i].classList.remove('last-card');
             cardContainerElem[i].style.background = 'rgba(255, 255, 255, 1)';;
             cardContainerElem[i].style.backgroundImage = 'unset';
-            // pElem.style.maskImage = 'unset';
-            // pElem.style.webkitMaskImage = 'unset';
+            pElem.style.maskImage = 'unset';
+            pElem.style.webkitMaskImage = 'unset';
         }
     } 
 
